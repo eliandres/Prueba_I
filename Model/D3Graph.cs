76a1f178ed5fa16788d3c22c281f7a4936052b66
eliandres <ebriceo92g@gmail.com>
2024-10-30 10:55:02 -0600

    Esta rpueba fue realizada para el relacion de exito de clonacion romota

--- Model/D3Graph.cs
+++ Model/D3Graph.cs
@@ -6,4 +6,4 @@ public record D3Graph(IEnumerable<D3Node> Nodes, IEnumerable<D3Link> Links);
 
 public record D3Node(string Title, string Label);
 
-public record D3Link(int Source, int Target);
+public record D3Link(int Source, int Target);///Esto es una modificacion 
